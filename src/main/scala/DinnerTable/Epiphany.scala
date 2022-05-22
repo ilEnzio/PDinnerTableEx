ba73f6d82e3070d4f46ef540320fea6955f32881
ilEnzio <westfury@gmail.com>
2022-05-22 00:21:40 -0400

    feat(epiph): shift a single Paradigm given the correct fork state

--- src/main/scala/DinnerTable/Epiphany.scala
+++ src/main/scala/DinnerTable/Epiphany.scala
@@ -51,13 +51,43 @@ Workflow: "Paradigm Shift"
   ) {}
 
   sealed trait ParadigmState
-  case object StartedEating extends ParadigmState
-  case object StartedThinking extends ParadigmState
-  case object StartedTalking extends ParadigmState
+  case object Eating extends ParadigmState
+  case object Thinking extends ParadigmState
+  case object Talking extends ParadigmState
 
   sealed trait ForkState
-  case object ForkGrabbed extends ForkState
-  case object ForkState extends ForkState
+  case object Grasped extends ForkState
+  case object Free extends ForkState
+
+  sealed trait ForkStateEvent
+  case object ForkGrabbed extends ForkStateEvent
+  case object ForkDropped extends ForkStateEvent
+
+  sealed trait ParadigmStateEvents
+  case object StartedEating extends ParadigmStateEvents
+  case object StartedThinking extends ParadigmStateEvents
+  case object StartedTalking extends ParadigmStateEvents
+
+  /*
+   * this should handle ShiftParadigm commands
+   *
+   * ParadigmState => (ForkState, ForkState) => ParadigmState
+   *
+   */
+
+  def shiftParadigm(
+      state: ParadigmState,
+      forks: (ForkState, ForkState)
+  ): Option[ParadigmState] = state match {
+    case Eating if forks == (Grasped, Grasped) => Some(Thinking)
+    case Thinking                              => Some(Talking)
+    case Talking if forks == (Free, Free)      => Some(Eating)
+    case _                                     => None
+  }
+
+  // Option allows me to fail fast, right?
+  // so if I get an Epiphany (a group of paradigm shifts) that doesn't
+  // work the idea is to get a none back into terms of shiftEvents
 
   def main(args: Array[String]): Unit = {
     println("test")
