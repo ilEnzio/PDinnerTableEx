9b37177f650a0db6a4d6d70c6cc64c441319c2a4
ilEnzio <westfury@gmail.com>
2022-05-21 22:27:50 -0400

    docs(Epiphany): document the state shift workflow

--- src/main/scala/DinnerTable/Epiphany.scala
+++ src/main/scala/DinnerTable/Epiphany.scala
@@ -2,7 +2,46 @@ package DinnerTable
 
 object Epiphany {
 
+/*
+The requirements mostly come from the wiki page:
+https://en.wikipedia.org/wiki/Dining_philosophers_problem
 
+5 Philosophers (Heraclitus, Searle, Nietzsche, Aristotle, McLuhan)
+5 Forks (??Dependencies?? Resources??)
+Philosophers States: Eating, Thinking, Talking(Hungry)
+
+
+Event Storm -
+  EphinanyReached - an event which triggers state changes for all Philosophers and
+    possible a Fork workflow event.
+
+  StartedEating
+  StartedThinking
+  StartedTalking
+
+  ForkGrabbed
+  ForkDropped
+
+
+Bounded Context: Shifting States/Paradigms
+Workflow: "Paradigm Shift"
+  triggered by:
+    "EpiphanyReached" event
+  primary input:
+    ?? The philosophers seating order? or No input
+  other input:
+    the Fork status
+
+   output events:
+      "<State Change>" events
+      "<Fork Change>" events
+
+   side-effects:
+      None?? Maybe so logs or displays.
+
+
+
+ */
 
 
 
